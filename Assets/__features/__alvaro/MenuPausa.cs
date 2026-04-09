6618adf3f8469e1ed0444c4ad941ea12c3923d4f
alvarobagr <alvaro230604@gmail.com>
2026-04-09 17:32:40 +0200

    menus

---
+++ Assets/__features/__alvaro/MenuPausa.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuPausa : MonoBehaviour
+{
+
+    public GameObject menuPausa;
+    private bool isGamePaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Escape))
+        {
+            isGamePaused = !isGamePaused;
+            PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        if (isGamePaused)
+        {
+            Time.timeScale = 0;
+
+            menuPausa.SetActive(true);
+
+           
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Time.timeScale = 1;
+
+            menuPausa.SetActive(false);
+
+            
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+    public void Continuar()
+    {
+        isGamePaused = false;
+        PauseGame();
+    }
+
+    public void Salir()
+    {
+        Time.timeScale = 1;
+        //SceneManager.LoadScene("MenuPrincipal");  //No se que opcion es mejor, de mometo lo dejo asi
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
